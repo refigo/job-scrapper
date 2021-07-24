4eac970ac5bcb9fe03b29e48ad1b8472e356b65e
Refi Go <64242999+refigo@users.noreply.github.com>
2021-07-24 14:53:38 +0900

    Clean up indeed code

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://kr.indeed.com/jobs?q=python&limit={LIMIT}&radius=25"
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pagination = soup.find("div", {"class":"pagination"})
@@ -30,10 +30,10 @@ def extract_job(html):
         'link': f"https://kr.indeed.com/viewjob?jk={job_id}"
         }
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        print(f"Scrapping page {page}")
+        print(f"Scrapping page {page+1}")
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all('a', {"class":"tapItem"})
@@ -42,4 +42,8 @@ def extract_indeed_jobs(last_page):
             jobs.append(job)
     return jobs
 
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
+    return jobs
         
