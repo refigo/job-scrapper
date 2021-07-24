e3046da1acdc951a7ce1f1c2047300e72adc54d1
Refi Go <64242999+refigo@users.noreply.github.com>
2021-07-24 20:46:10 +0900

    Update exportion csv file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
+from save import save_to_file
 
 app = Flask("SuperPythonScrapper")
 
@@ -28,5 +29,20 @@ def report():
     jobs=jobs
     )
 
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
+    
 app.run("0.0.0.0")
 
