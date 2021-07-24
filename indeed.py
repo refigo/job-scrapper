354b65ab296e6f1ede49822848057636a0ad3e6a
Refi Go <64242999+refigo@users.noreply.github.com>
2021-07-24 16:31:53 +0900

    Merge indeed and SO

--- indeed.py
+++ indeed.py
@@ -33,7 +33,7 @@ def extract_job(html):
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        print(f"Scrapping page {page+1}")
+        print(f"Scrapping indeed: Page {page+1}")
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all('a', {"class":"tapItem"})
